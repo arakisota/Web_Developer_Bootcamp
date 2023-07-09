c00ede2048b648979a9de8f268791872d95be628
arakisota <soutaraki0126@gmail.com>
2023-07-09 21:52:01 +0900

    if文に変更

--- Section14/app.js
+++ Section14/app.js
@@ -1,3 +1,8 @@
-console.log('はじめてのJavaScriptファイル!');
-let total = 1+4;
-console.log("GoodBye!!!");
+//const dayOfWeek = "Monday";
+const dayOfWeek = prompt("英語で入力してください").toLowerCase();
+
+if (dayOfWeek === "monday") {
+    console.log("月曜日は憂鬱");
+} else if (dayOfWeek === "saturday") {
+    console.log('土曜日は最高！');
+}
